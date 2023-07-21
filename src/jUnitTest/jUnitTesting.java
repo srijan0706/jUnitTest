023545cbf932ebcc6df3f1f7a5498d5930c794d7
Srijan <Srijan@Srijan-OMEN-15>
2023-07-21 09:08:13 +0530

    Added two functions, AddNumbers and AddStrings

---
+++ src/jUnitTest/jUnitTesting.java
@@ -0,0 +1,11 @@
+package jUnitTest;
+public class jUnitTesting {
+	public int AddNumbers(int a, int b)
+	{
+		return a+b;
+	}
+	public String AddStrings(String a,String b)
+	{
+		return a.concat(b);
+	}
+}
